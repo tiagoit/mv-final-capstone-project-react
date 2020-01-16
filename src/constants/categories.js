52f3e06547e4a39d6fc44d6368e4a55f10d0d93a
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-15 22:16:15 -0300

    add categories contants and helpers file

---
+++ src/constants/categories.js
@@ -0,0 +1,8 @@
+const categories = {
+  events: ['waitres', 'barman', 'dj', 'security'],
+  'domestic-services': ['contractors', 'eletricians', 'home-cleaners', 'plumbers', 'landscaping'],
+  health: ['spa', 'gyms', 'psychologis'],
+  beauty: ['hair', 'massage'],
+};
+
+export default categories;
