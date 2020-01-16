14c77f5cfc7bb75fc711eec326b8fa983b6bcf12
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-15 23:08:20 -0300

    feat: add login and register components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [state, setState] = React.useState({ email: '' });
+
+  function handleChange(ev) {
+    const value = ev.target.type === 'checkbox' ? ev.target.checked : ev.target.value;
+    setState({ ...state, [ev.target.name]: value });
+  }
+
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(state);
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="email" placeholder="Email" onChange={handleChange}/>
+        <button type="submit">Login</button>
+      </form>
+      New user? <Link to="/register">Register</Link>
+    </div>
   );
 }
