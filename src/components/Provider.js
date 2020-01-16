2c6cb0f8fe5e2f4148dc55d49cc7ff6480ab8d97
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-16 13:48:32 -0300

    add message form to service compoent

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -2,12 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Provider(props) {
+  const [state, setState] = React.useState({ message: '' });
   const { name, photo } = props;
+
+  function handleChange(ev) {
+    setState({ ...state, [ev.target.name]: ev.target.value });
+  }
+
+  function handleMessage() {
+    console.log(state.message);
+  }
+
   return (
     <div>
       {name}
       <img src={photo} alt={name} />
-      <button type="button">Message</button>
+      <input type="text" name="messsage" onChange={handleChange} />
+      <button type="button" onClick={handleMessage}>Send</button>
     </div>
   );
 }
