74cb6da4c742f5491043573e7aae7b8f9b6f6484
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-16 21:53:12 -0300

    feat: connect favourites with redux store

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import toggleFavouriteAction from '../redux/actions';
 import Provider from './Provider';
 import providers from '../constants/providers';
 
-const Login = () => (
+const Favourites = ({ favourites }) => (
   <div>
     <h2>Favourite providers</h2>
-    {providers.map(p => <Provider key={p.id} provider={p} />)}
+    {providers
+      .filter(p => favourites.includes(p.id))
+      .map(p => <Provider key={p.id} provider={p} />)}
   </div>
 );
-export default Login;
+
+Favourites.propTypes = {
+  favourites: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = (state) => ({ favourites: state.providers.favourites });
+const mapDispatchToProps = (dispatch) => ({
+  toggleFavourite: (id) => dispatch(toggleFavouriteAction(id)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Favourites);
