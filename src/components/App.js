de89cb1e92a48999244452cacfbecb275feeeb1f
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-17 22:21:48 -0300

    feat: install material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link as RouterLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { AppBar, Container, Box, Button, Toolbar, Typography, IconButton, Link, Menu, MenuItem } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
 
 import './App.scss';
 import Home from './Home';
@@ -11,6 +13,9 @@ import Favourites from './Favourites';
 import { logoutAction, loginAction } from '../redux/actions';
 
 const App = ({ isLoggedIn, user, rxLogin, rxLogout }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
   if (!isLoggedIn && localStorage.getItem('token')) {
     rxLogin({
       name: localStorage.getItem('name'),
@@ -19,36 +24,73 @@ const App = ({ isLoggedIn, user, rxLogin, rxLogout }) => {
     });
   }
 
+  const handleMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleLogout = () => {
+    handleMenuClose();
     localStorage.removeItem('token');
     localStorage.removeItem('name');
     localStorage.removeItem('email');
     rxLogout();
   };
 
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id="user-menu"
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>{isLoggedIn && user.name}</MenuItem>
+      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+    </Menu>
+  );
+
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className="title">
+            <Link component={RouterLink} to="/" color="inherit">Providers directory</Link>
+          </Typography>
+          {!isLoggedIn && <Button color="inherit" component={RouterLink} to="/login">Login</Button>}
+          {!isLoggedIn && <Button color="inherit" component={RouterLink} to="/register">New user</Button>}
 
-            {!isLoggedIn && <li><Link to="/login">Login</Link></li>}
-            {!isLoggedIn && <li><Link to="/register">New user</Link></li>}
+          {isLoggedIn && <Button color="inherit" component={RouterLink} to="/favourites">Favourites</Button>}
+          {isLoggedIn && (
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls="user-menu"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            ><AccountCircle />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+      {isLoggedIn && renderMenu}
 
-            {isLoggedIn && <li><Link to="/favourites">Favourite providers</Link></li>}
-            {isLoggedIn && <li>{user.name}</li>}
-            {isLoggedIn && <li><button type="button" onClick={handleLogout}>Logout</button></li>}
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/favourites"><Favourites /></Route>
-          <Route path="/login"><Login /></Route>
-          <Route path="/register"><Register /></Route>
-          <Route path="/"><Home /></Route>
-        </Switch>
-      </div>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Switch>
+            <Route path="/favourites"><Favourites /></Route>
+            <Route path="/login"><Login /></Route>
+            <Route path="/register"><Register /></Route>
+            <Route path="/"><Home /></Route>
+          </Switch>
+        </Box>
+      </Container>
     </Router>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+import store from './redux/store';
 import './index.scss';
 import App from './components/App';
+import theme from './theme';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
